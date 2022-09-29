195d1a1dce15dbcf3450313df1c19cc268bb33a5
Teemu Heino <teemu_heino@outlook.com>
2022-09-29 09:57:04 +0300

    Add back button

--- src/features/register/Register.tsx
+++ src/features/register/Register.tsx
@@ -1,4 +1,6 @@
-import { Typography } from '@mui/material';
+import { ArrowBackIos } from '@mui/icons-material';
+import { IconButton, Typography } from '@mui/material';
+import Grid2 from '@mui/material/Unstable_Grid2';
 import { SubmitHandler } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import RegistrationForm from '../../components/registration-form/RegistrationForm';
@@ -11,10 +13,23 @@ const Register = (): JSX.Element => {
         console.log(data);
     };
 
+    // TODO Replace the div with the Login & Registration page layout
     return (
-        <div>
-            <Typography variant="h1">{t('registration')}</Typography>
-            <RegistrationForm onSubmit={onSubmit} />
+        <div style={{ width: '500px' }}>
+            <Grid2 container spacing={0}>
+                <Grid2 xs={1}>
+                    <IconButton
+                        color="primary"
+                        aria-label="return to previous page"
+                    >
+                        <ArrowBackIos />
+                    </IconButton>
+                </Grid2>
+                <Grid2 xs={11}>
+                    <Typography variant="h1">{t('registration')}</Typography>
+                    <RegistrationForm onSubmit={onSubmit} />
+                </Grid2>
+            </Grid2>
         </div>
     );
 };
