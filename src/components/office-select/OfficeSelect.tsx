80a04fbb3db726f1f16777d78a80891b344da626
Teemu Heino <teemu_heino@outlook.com>
2022-09-29 09:38:09 +0300

    Add office select as reusable component

---
+++ src/components/office-select/OfficeSelect.tsx
@@ -0,0 +1,32 @@
+import { Select, MenuItem, SelectProps } from '@mui/material';
+import { ForwardedRef, forwardRef } from 'react';
+
+type OfficeSelectProps = SelectProps;
+
+const OfficeSelect = (
+    props: OfficeSelectProps,
+    ref: ForwardedRef<unknown>
+): JSX.Element => {
+    // TODO Fetch these from backend
+    const DUMMY_OFFICES: Array<{ id: number; name: string }> = [
+        {
+            id: 1,
+            name: 'Office 1',
+        },
+    ];
+
+    return (
+        <Select {...props} ref={ref}>
+            <MenuItem value={-1} key={-1}>
+                -
+            </MenuItem>
+            {DUMMY_OFFICES.map((it) => (
+                <MenuItem value={it.id} key={it.id}>
+                    {it.name}
+                </MenuItem>
+            ))}
+        </Select>
+    );
+};
+
+export default forwardRef(OfficeSelect);
