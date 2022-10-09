d24fa69303e7e3db8b5c5faad0686b0396b83123
Roger Wanamo <roger.wanamo@tuni.fi>
2022-10-09 15:14:53 +0300

    updated theme styles, added responsive styles to login layout

--- src/theme.ts
+++ src/theme.ts
@@ -5,22 +5,28 @@ const defaultTheme = createTheme(); // Create a default theme so we can use brea
 
 const HuldTheme = createTheme({
     typography: {
+        fontSize: 17,
         fontFamily: "'Fira Sans', sans-serif",
-        fontSize: 20,
         h1: {
+            fontFamily: "'trebuchet ms', 'Fira Sans', sans-serif",
             fontSize: '3.5rem',
+            fontWeight: 'bold',
             [defaultTheme.breakpoints.down('sm')]: {
                 fontSize: '2rem',
             },
         },
         h2: {
+            fontFamily: "'trebuchet ms', 'Fira Sans', sans-serif",
             fontSize: '2rem',
+            fontWeight: 'bold',
             [defaultTheme.breakpoints.down('sm')]: {
                 fontSize: '1.5rem',
             },
         },
         h3: {
+            fontFamily: "'trebuchet ms', 'Fira Sans', sans-serif",
             fontSize: '1.5rem',
+            fontWeight: 'bold',
         },
     },
     palette: {
@@ -48,12 +54,16 @@ const HuldTheme = createTheme({
     },
     spacing: 8, // Defines pixel scaling -> theme.spacing(1) -> 8px -> space-xxs
     // Style overrides for specific components go here
+
+    shape: {
+        borderRadius: 0,
+    },
     components: {
         MuiFormLabel: {
             styleOverrides: {
-                root: ({ ownerState }) => ({
+                root: ({ ownerState, theme }) => ({
                     ...(ownerState.color === 'primary' && {
-                        color: '#05133d',
+                        color: theme.palette.text.primary,
                     }),
                 }),
             },
@@ -64,6 +74,23 @@ const HuldTheme = createTheme({
             defaultProps: {
                 component: LinkBehaviour,
             } as LinkProps,
+        },
+        MuiOutlinedInput: {
+            styleOverrides: {
+                notchedOutline: ({ theme }) => ({
+                    borderColor: theme.palette.action.disabledBackground,
+                }),
+            },
+        },
+        MuiCheckbox: {
+            styleOverrides: {
+                colorSecondary: ({ theme }) => ({
+                    color: theme.palette.primary.light,
+                    '&$checked': {
+                        color: theme.palette.primary.main,
+                    },
+                }),
+            },
         },
     },
 });
