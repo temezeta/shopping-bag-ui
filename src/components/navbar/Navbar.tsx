e1c6cba8b6c74106b2bf3a16d3e20c21dcf8ccd2
Roger Wanamo <roger.wanamo@tuni.fi>
2022-10-09 19:36:14 +0300

    Removed a few unnecessary css-lines

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,11 +6,7 @@ import AppLogo from '../logo/AppLogo';
 const Navbar = (): JSX.Element => {
     return (
         <Box bgcolor="primary.dark" color="white">
-            <Grid2
-                container
-                className={styles.header}
-                justifyContent="space-between"
-            >
+            <Grid2 container className={styles.header}>
                 <Grid2 xs={12} sm={6} className={styles.headerLogo}>
                     <AppLogo />
                 </Grid2>
