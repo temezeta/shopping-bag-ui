3c95df1dfc86578ab27fa0ea3c4ea9cb180d1d25
Roger Wanamo <roger.wanamo@tuni.fi>
2022-10-09 19:31:30 +0300

    Added navbar, svg-logo and an empty /item-page to preview navbar

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from './features/login/Login';
 import Register from './features/register/Register';
+import ItemDetails from './features/item-details/ItemDetails';
 
 const AppRouter = (): JSX.Element => {
     return (
         <Routes>
             <Route path="register" element={<Register />}></Route>
             <Route path="login" element={<Login />}></Route>
+            <Route path="item" element={<ItemDetails />}></Route>
         </Routes>
     );
 };
