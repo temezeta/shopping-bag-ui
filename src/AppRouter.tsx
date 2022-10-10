3f05732f707d3a350ec14fa7d8ce359fb900f72a
LonelyRick <106442252+LonelyRick@users.noreply.github.com>
2022-10-10 18:13:16 +0200

    Login page (#25)
    * Modify previous changes
    
    Auth commit
    
    Requested changes
    
    Add checkbox
    
    Second Commit
    
    LoginDto creation
    
    Started
    
    * Fix merge conflicts.
    
    Co-authored-by: Teemu Heino <teemu_heino@outlook.com>

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
+import Login from './features/login/Login';
 import Register from './features/register/Register';
 
 const AppRouter = (): JSX.Element => {
     return (
         <Routes>
             <Route path="register" element={<Register />}></Route>
+            <Route path="login" element={<Login />}></Route>
         </Routes>
     );
 };
