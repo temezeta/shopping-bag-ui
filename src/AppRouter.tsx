4f5407684f1084a7da25bf4352b6e9e1c1fa417e
Teemu Heino <teemu_heino@outlook.com>
2022-10-11 18:04:37 +0300

    Modify AuthGuard

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,6 +1,5 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import AuthGuard from './components/auth-guard/AuthGuard';
-import SessionGuard from './components/session-guard/SessionGuard';
 import Login from './features/login/Login';
 import Register from './features/register/Register';
 import ItemDetails from './features/item-details/ItemDetails';
@@ -8,9 +7,8 @@ import ItemDetails from './features/item-details/ItemDetails';
 const AppRouter = (): JSX.Element => {
     return (
         <Routes>
-            {/** Restores session from local storage if possible */}
-            <Route path="" element={<SessionGuard />}></Route>
             {/** Unprotected routes */}
+            <Route path="" element={<Navigate to="login" />}></Route>
             <Route path="register" element={<Register />}></Route>
             <Route path="login" element={<Login />}></Route>
             {/** Protected routes */}
--- src/components/auth-guard/AuthGuard.tsx
+++ src/components/auth-guard/AuthGuard.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { selectCurrentUser } from '../../store/user/user-slice';
@@ -9,27 +10,31 @@ interface AuthGuardProps {
 const AuthGuard = (props: AuthGuardProps): JSX.Element => {
     const currentUser = useSelector(selectCurrentUser);
     const location = useLocation();
+    const { t } = useTranslation();
 
     /**
-     * Checks if user is logged in AND if role is required, checks that user has that role
+     * 1. If user is not logged in -> redirect to login
+     * 2. If user is logged in but doesn't have appropriate role -> show error
+     * 3. If user has appropriate role -> show page
      */
-    const hasAccess = (): boolean => {
+    const getAuthorizedRoute = (): JSX.Element => {
         const requiredRoles = props.roles;
         if (!currentUser) {
-            return false;
-        } else if (requiredRoles) {
-            return currentUser.userRoles.some((it) =>
-                requiredRoles.includes(it.roleName)
+            return (
+                <Navigate to={'/login'} state={{ from: location }} replace />
             );
+        } else if (
+            requiredRoles &&
+            !currentUser.userRoles.some((it) =>
+                requiredRoles.includes(it.roleName)
+            )
+        ) {
+            return <div>{t('errors.access_denied')}</div>;
         }
-        return true;
+        return <Outlet />;
     };
 
-    return hasAccess() ? (
-        <Outlet />
-    ) : (
-        <Navigate to={''} state={{ from: location }} replace />
-    );
+    return getAuthorizedRoute();
 };
 
 export default AuthGuard;
