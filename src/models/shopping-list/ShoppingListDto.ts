92914e799b317f6881cd77e4293e9208f5fa394c
Teemu Heino <teemu_heino@outlook.com>
2022-10-19 14:10:50 +0300

    Modify ShoppingListDto to match backend.

--- src/models/shopping-list/ShoppingListDto.ts
+++ src/models/shopping-list/ShoppingListDto.ts
@@ -3,11 +3,11 @@ import { ItemDto } from './ItemDto';
 export interface ShoppingListDto {
     id: number;
     name: string;
-    comment: string;
+    comment?: string;
     ordered: boolean;
     createdDate: string;
-    startDate: string;
-    dueDate: string;
-    expectedDeliveryDate: string;
+    startDate?: string;
+    dueDate?: string;
+    expectedDeliveryDate?: string;
     items: ItemDto[];
 }
