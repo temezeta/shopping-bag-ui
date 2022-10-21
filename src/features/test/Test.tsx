3c30d60724fa122cb9715e0418e3186fc401bd7b
Roni Niskanen <roni.niskanen@tuni.fi>
2022-10-21 17:29:41 +0300

    First versions of shopping lists item and shopping lists table

---
+++ src/features/test/Test.tsx
@@ -0,0 +1,18 @@
+import ShoppingListsTable from '../../components/shopping-lists-table/ShoppingListsTable';
+// import ShoppingListTab from '../../components/shopping-list-tab/ShoppingListTab';
+// import { ShoppingListDto } from '../../models/shopping-list/ShoppingListDto';
+// import {
+//    getShoppingListsByOfficeAsync,
+//    selectActiveLists,
+//    selectInactiveLists,
+// } from '../../store/shopping-list/shopping-list-slice';
+
+const Test = (): JSX.Element => {
+    return (
+        <div>
+            <ShoppingListsTable></ShoppingListsTable>
+        </div>
+    );
+};
+
+export default Test;
