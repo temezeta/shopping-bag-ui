0f11e29b1cae898323b83d3db5a8facd1324afeb
Maija Pietilä <pietilamai@gmail.com>
2022-10-26 22:53:07 +0300

    Change url address

--- src/store/shopping-list-item/shopping-list-item-actions.ts
+++ src/store/shopping-list-item/shopping-list-item-actions.ts
@@ -3,7 +3,7 @@ import ApiClient from '../client';
 export const removeShoppingListItem = async (
     itemId: number
 ): Promise<boolean> => {
-    const response = await ApiClient.delete(`shoppinglistitem`, itemId);
+    const response = await ApiClient.delete(`Item/${itemId}`, '');
     if (!response.ok) {
         return false;
     }
