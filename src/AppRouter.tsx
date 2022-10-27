c06966ebf5f8b2db611610e1315b62bad4fbf89e
LonelyRick <alexruizballester@gmail.com>
2022-10-27 16:49:32 +0300

    Resolve merge conflicts

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from './features/login/Login';
 import Register from './features/register/Register';
+import AddShoppingList from './features/shopping-lists/AddShoppingList';
+import { Role } from './models/user/RoleEnum';
 import ItemDetails from './features/item-details/ItemDetails';
 import AddItem from './features/add-item/AddItem';
 
@@ -9,6 +11,17 @@ const AppRouter = (): JSX.Element => {
         <Routes>
             <Route path="register" element={<Register />}></Route>
             <Route path="login" element={<Login />}></Route>
+            {/** Protected routes */}
+            <Route element={<AuthGuard />}>
+                <Route path="home" element={<Home />}></Route>
+            </Route>
+            {/** Admin routes */}
+            <Route element={<AuthGuard roles={[Role.Admin]} />}>
+                <Route
+                    path="addshoppinglist"
+                    element={<AddShoppingList />}
+                ></Route>
+            </Route>
             <Route path="item" element={<ItemDetails />}></Route>
             <Route path="addItem/:id" element={<AddItem />}></Route>
         </Routes>
