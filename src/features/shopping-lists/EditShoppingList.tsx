80ee93e070658a8f74c2a408a75533befe741b03
Teemu Heino <teemu_heino@outlook.com>
2022-10-27 13:46:59 +0300

    Add confirmation dialogs for edit & remove operations.

--- src/features/shopping-lists/EditShoppingList.tsx
+++ src/features/shopping-lists/EditShoppingList.tsx
@@ -1,5 +1,5 @@
 import { ArrowBackIos } from '@mui/icons-material';
-import { IconButton, Typography } from '@mui/material';
+import { DialogContentText, IconButton, Typography } from '@mui/material';
 import Grid2 from '@mui/material/Unstable_Grid2';
 import { SubmitHandler } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
@@ -15,6 +15,8 @@ import {
 import { RootState } from '../../store/store';
 import { ModifyShoppingListDto } from '../../models/shopping-list/ModifyShoppingListDto';
 import { ShoppingListDto } from '../../models/shopping-list/ShoppingListDto';
+import { useState } from 'react';
+import ConfirmationDialog from '../../components/confirmation-popup/ConfirmationDialog';
 
 const EditShoppingList = (): JSX.Element => {
     const { listId } = useParams();
@@ -22,49 +24,99 @@ const EditShoppingList = (): JSX.Element => {
     const { t } = useTranslation();
     const navigate = useNavigate();
 
+    const [modification, setModification] = useState<
+        ModifyShoppingListDto | undefined
+    >(undefined);
+    const [isModifyOpen, setModifyOpen] = useState<boolean>(false);
+    const [toDelete, setToDelete] = useState<ShoppingListDto | undefined>(
+        undefined
+    );
+    const [isDeleteOpen, setDeleteOpen] = useState<boolean>(false);
+
     // TODO Fetch fresh version of shopping list on mount when endpoint available
     const shoppingList = useAppSelector((state: RootState) =>
         selectShoppingListById(state, parseInt(listId as string))
     );
 
     const onSubmit: SubmitHandler<ModifyShoppingListDto> = async (data) => {
-        if (shoppingList) {
-            await dispatch(
-                modifyShoppingListAsync({ data, listId: shoppingList.id })
-            ).unwrap();
+        setModification(data);
+        setModifyOpen(true);
+    };
+
+    const onDelete: SubmitHandler<ShoppingListDto> = async (data) => {
+        setToDelete(data);
+        setDeleteOpen(true);
+    };
+
+    const onDeleteConfirm = async (): Promise<void> => {
+        if (toDelete) {
+            await dispatch(removeShoppingListAsync(toDelete)).unwrap();
+            setToDelete(undefined);
+            setDeleteOpen(false);
             navigate('/home');
         }
     };
 
-    const onDelete: SubmitHandler<ShoppingListDto> = async (data) => {
-        await dispatch(removeShoppingListAsync(data)).unwrap();
-        navigate('/home');
+    const onModifyConfirm = async (): Promise<void> => {
+        if (modification && shoppingList) {
+            await dispatch(
+                modifyShoppingListAsync({
+                    data: modification,
+                    listId: shoppingList.id,
+                })
+            ).unwrap();
+            setModification(undefined);
+            setModifyOpen(false);
+            navigate('/home');
+        }
     };
 
     return (
-        <MainLayout width="35em">
-            <Grid2 container spacing={1}>
-                <Grid2 xs={12} className="flex-center">
-                    <IconButton onClick={() => navigate('/home')}>
-                        <ArrowBackIos />
-                    </IconButton>
-                    <Typography
-                        variant="h1"
-                        display="flex"
-                        justifyContent="center"
-                    >
-                        {t('actions.edit_list')}
-                    </Typography>
+        <>
+            <MainLayout width="35em">
+                <Grid2 container spacing={1}>
+                    <Grid2 xs={12} className="flex-center">
+                        <IconButton onClick={() => navigate('/home')}>
+                            <ArrowBackIos />
+                        </IconButton>
+                        <Typography
+                            variant="h1"
+                            display="flex"
+                            justifyContent="center"
+                        >
+                            {t('actions.edit_list')}
+                        </Typography>
+                    </Grid2>
+                    <Grid2 xs={12}>
+                        <ShoppingListForm
+                            initialValues={shoppingList}
+                            onSubmit={onSubmit}
+                            onDelete={onDelete}
+                        />
+                    </Grid2>
                 </Grid2>
-                <Grid2 xs={12}>
-                    <ShoppingListForm
-                        initialValues={shoppingList}
-                        onSubmit={onSubmit}
-                        onDelete={onDelete}
-                    />
-                </Grid2>
-            </Grid2>
-        </MainLayout>
+            </MainLayout>
+            <ConfirmationDialog
+                open={isModifyOpen}
+                onConfirm={onModifyConfirm}
+                onCancel={() => setModifyOpen(false)}
+                title={t('actions.edit_list')}
+            >
+                <DialogContentText>
+                    {t('dialogs.confirmation')}
+                </DialogContentText>
+            </ConfirmationDialog>
+            <ConfirmationDialog
+                open={isDeleteOpen}
+                onConfirm={onDeleteConfirm}
+                onCancel={() => setDeleteOpen(false)}
+                title={t('actions.remove_list')}
+            >
+                <DialogContentText>
+                    {t('dialogs.confirmation')}
+                </DialogContentText>
+            </ConfirmationDialog>
+        </>
     );
 };
 
