3b1abe5d62996451460a88484c687b3c746df9e7
Maija Pietilä <pietilamai@gmail.com>
2022-10-28 18:23:49 +0300

    Change file naming conventions

--- src/store/lists/item-actions.ts
+++ src/store/lists/item-actions.ts
@@ -4,9 +4,5 @@ export const removeShoppingListItem = async (
     itemId: number
 ): Promise<boolean> => {
     const response = await ApiClient.delete(`Item/${itemId}`, '');
-    if (!response.ok) {
-        return false;
-    }
-
-    return true;
+    return response.ok;
 };
