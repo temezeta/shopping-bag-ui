bf22cc3030e304636ad4662f2c873c67c8b7eb03
LonelyRick <alexruizballester@gmail.com>
2022-11-07 18:18:14 +0200

    Try to fix

--- src/features/admin-shopping-list/AdminShoppingList.tsx
+++ src/features/admin-shopping-list/AdminShoppingList.tsx
@@ -8,8 +8,9 @@ import { RootState } from '../../store/store';
 import {
     selectShoppingListById,
     selectActiveLists,
+    getShoppingListByIdAsync,
 } from '../../store/shopping-list/shopping-list-slice';
-import { useAppSelector } from '../../store/hooks';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import ShoppingListTab from '../../components/shopping-list-tab/ShoppingListTab';
 import {
     selectCurrentOffice,
@@ -17,58 +18,70 @@ import {
 } from '../../store/user/user-slice';
 import { Role } from '../../models/user/RoleEnum';
 import AdminShoppingListTab from '../../components/shopping-list-tab/AdminShoppingListTab';
+import { useEffect } from 'react';
+
 const AdminShoppingList = (): JSX.Element => {
     const { listId } = useParams();
     const id = Number(listId);
     // const { t } = useTranslation();
-    const shoppingList = useAppSelector((state: RootState) =>
-        selectShoppingListById(state, id)
-    );
     const navigate = useNavigate();
-    const header = shoppingList?.name;
     const shoppingLists = useAppSelector(selectActiveLists);
     const user = useAppSelector(selectCurrentUser);
     const currentOffice = useAppSelector(selectCurrentOffice);
+    const dispatch = useAppDispatch();
+    useEffect(() => {
+        void dispatch(getShoppingListByIdAsync(id));
+    }, []);
+    const shoppingList = useAppSelector((state: RootState) =>
+        selectShoppingListById(state, id)
+    );
+    const header = shoppingList?.name;
     return (
-        <MainLayout>
-            <Grid2 container spacing={2}>
-                <Grid2 xs={12} className="flex-center">
-                    <IconButton onClick={() => navigate('/shopping-lists')}>
-                        <ArrowBackIos />
-                    </IconButton>
-                    <Typography
-                        variant="h1"
-                        display="flex"
-                        justifyContent="center"
-                    >
-                        {header}
-                    </Typography>
+        <>
+            <MainLayout>
+                <Grid2 container spacing={2}>
+                    <Grid2 xs={12} className="flex-center">
+                        <IconButton onClick={() => navigate('/shopping-lists')}>
+                            <ArrowBackIos />
+                        </IconButton>
+                        <Typography
+                            variant="h1"
+                            display="flex"
+                            justifyContent="center"
+                        >
+                            {header}
+                        </Typography>
+                    </Grid2>
+                    {/* show different views depending the user */}
+                    <div>
+                        {id &&
+                            user?.userRoles.some(
+                                (it) => it.roleName === Role.User
+                            ) &&
+                            shoppingLists.map((list, i) => (
+                                <ShoppingListTab
+                                    value={id}
+                                    list={list}
+                                    key={i}
+                                />
+                            ))}
+                        {id &&
+                            currentOffice &&
+                            user?.userRoles.some(
+                                (it) => it.roleName === Role.Admin
+                            ) &&
+                            shoppingLists.map((list, i) => (
+                                <AdminShoppingListTab
+                                    value={id}
+                                    list={list}
+                                    key={i}
+                                    office={currentOffice}
+                                />
+                            ))}
+                    </div>
                 </Grid2>
-                {/* show different views depending the user */}
-                <div>
-                    {id &&
-                        user?.userRoles.some(
-                            (it) => it.roleName === Role.User
-                        ) &&
-                        shoppingLists.map((list, i) => (
-                            <ShoppingListTab value={id} list={list} key={i} />
-                        ))}
-                    {id &&
-                        currentOffice &&
-                        user?.userRoles.some(
-                            (it) => it.roleName === Role.Admin
-                        ) &&
-                        shoppingLists.map((list, i) => (
-                            <AdminShoppingListTab
-                                value={id}
-                                list={list}
-                                key={i}
-                                office={currentOffice}
-                            />
-                        ))}
-                </div>
-            </Grid2>
-        </MainLayout>
+            </MainLayout>
+        </>
     );
 };
 export default AdminShoppingList;
