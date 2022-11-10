1ea4d60be890935fd644d63246b123d86204f787
Maija Pietilä <pietilamai@gmail.com>
2022-11-10 21:16:17 +0200

    Add textbox & close dialog icon, align components

--- src/components/delete-account-popup/DeleteAccountDialog.tsx
+++ src/components/delete-account-popup/DeleteAccountDialog.tsx
@@ -4,7 +4,11 @@ import {
     DialogActions,
     DialogContent,
     DialogTitle,
+    TextField,
+    FormLabel,
 } from '@mui/material';
+import Grid2 from '@mui/material/Unstable_Grid2';
+import CloseIcon from '@mui/icons-material/Close';
 import { useTranslation } from 'react-i18next';
 
 interface DeleteAccountgProps {
@@ -23,23 +27,51 @@ const DeleteAccountDialog = (props: DeleteAccountgProps): JSX.Element => {
             onClose={props.onCancel}
             aria-labelledby="delete-account-dialog-title"
         >
-            <DialogTitle
-                id="delete-account-dialog-title"
-                sx={{ textAlign: 'center' }}
-            >
-                {props.title}
-            </DialogTitle>
+            <Grid2>
+                <DialogTitle
+                    id="delete-account-dialog-title"
+                    sx={{ textAlign: 'center' }}
+                >
+                    {props.title}
+                    <CloseIcon
+                        fontSize="medium"
+                        onClick={props.onCancel}
+                        style={{
+                            float: 'right',
+                        }}
+                    />
+                </DialogTitle>
+            </Grid2>
             <DialogContent>{props.children}</DialogContent>
             <DialogActions sx={{ justifyContent: 'center' }}>
                 {props.onConfirm && (
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={props.onConfirm}
-                        autoFocus
+                    <Grid2
+                        container
+                        spacing={1}
+                        alignItems="flex-end"
+                        sx={{ padding: '1.5rem' }}
                     >
-                        {t('actions.delete')}
-                    </Button>
+                        <Grid2 xs={6} md={9}>
+                            <FormLabel id="type-delete">
+                                {t('dialogs.type_delete')}
+                            </FormLabel>
+                            <TextField
+                                aria-labelledby="type-delete"
+                                size="small"
+                                fullWidth
+                            />
+                        </Grid2>
+                        <Grid2 xs={6} md={3}>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                onClick={props.onConfirm}
+                                autoFocus
+                            >
+                                {t('actions.delete')}
+                            </Button>
+                        </Grid2>
+                    </Grid2>
                 )}
             </DialogActions>
         </Dialog>
